a2a5cfafb74c982d57d20c9f3336ca522a5c96ac
mithilesht <mithilesht@cloudthat.com>
2022-11-23 16:33:21 +0530

    added boilerplate cd ci code

--- test/ci-cd-aws-pipeline-demo.test.ts
+++ test/ci-cd-aws-pipeline-demo.test.ts
@@ -2,6 +2,7 @@
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as CiCdAwsPipelineDemo from '../lib/ci-cd-aws-pipeline-demo-stack';
 
+import { handler } from "../lib/lambda/handler";
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/ci-cd-aws-pipeline-demo-stack.ts
 test('SQS Queue Created', () => {
@@ -14,4 +15,9 @@ test('SQS Queue Created', () => {
 //   template.hasResourceProperties('AWS::SQS::Queue', {
 //     VisibilityTimeout: 300
 //   });
+});
+
+test('foo', async () => {
+    const result = await handler("", "");
+    expect(result.statusCode).toEqual(300);
 });
